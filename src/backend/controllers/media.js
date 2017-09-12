5920e5e120cbd8859e0304cc4427239b86438f8b
Nick Shtefan <nick.shtefan@gmail.com>
2017-09-12 20:29:59 +0300

    Реализация контроллера БД для Media. CRUD операции

--- src/backend/controllers/media.js
+++ src/backend/controllers/media.js
@@ -1,31 +1,61 @@
-const Media = require('../schemas/media')
-const Section = require('../controllers/section')
+const MediaSchema = require('../schemas/media')
+const Section = require('../schemas/section')
+const mongoose = require('mongoose');
 
 const createNewMedia = function (sectionID, Title, Description, done) {
 
-    let newMedia = new Media()
+    let mediaSchema = mongoose.model('Media', MediaSchema);
+    let newMedia = new mediaSchema()
 
     newMedia.Title = Title
-    newMedia.Tags = [{ URL: 'MEDIA-1', Text: 'MEDIA-TAG-1' }, { URL: 'MEDIA-2', Text: 'MEDIA-TAG-2' }, { URL: 'MEDIA-3', Text: 'MEDIA-TAG-3' }]
+    newMedia.Tags = [{ URL: 'MEDIA-1', Text: 'MEDIA-TAG-1' }, { URL: 'MEDIA-2', Text: 'MEDIA-TAG-2' }]
+    newMedia.URL = 'Media-URL'
 
-    Section.getSectionByID(sectionID, function (err, section) {
+    Section.findOneAndUpdate({ _id: sectionID }, { $push: { "Listing.Media": newMedia } }, function (err, section) {
         if (err)
             throw err
-        else {
-            section.update({ $push: { "Listing": newMedia } },
-                function (err, numAffected, rawResponse) {
-                    if (err)
-                        throw err
-                    else {
-                        newMedia.save(function (err) {
-                            if (err)
-                                throw err
-                            return done(null, newMedia)
-                        })
-                    }
-                })
-        }
+        else
+            return done(null, section)
     })
 }
 
-module.exports = { createNewMedia }
+const getMediaByID = function (sectionID, mediaID, done) {
+    Section
+        .findOne({ _id: sectionID }, function (err, section) {
+            if (err)
+                throw err
+            else {
+                let media = section.Listing.Media.id(mediaID);
+                return done(null, media)
+            }
+        })
+}
+
+const dropMediaByID = function (sectionID, mediaID, done) {
+    Section
+        .findOne({ _id: sectionID }, function (err, section) {
+            if (err)
+                throw err
+            else {
+                section.Listing.Media.id(mediaID).remove()
+                section.save()
+                return done(null, section)
+            }
+        })
+}
+
+const updateMediaByID = function (sectionID, mediaID, data, done) {
+    Section
+        .findOne({ _id: sectionID }, function (err, section) {
+            if (err)
+                throw err
+            else {
+                var media = section.Listing.Media.id(mediaID)
+                media.Title = data
+                section.save()
+                return done(null, media)
+            }
+        })
+}
+
+module.exports = { createNewMedia, getMediaByID, dropMediaByID, updateMediaByID }
