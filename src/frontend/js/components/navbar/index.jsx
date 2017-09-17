b8bd6215a99accb728d72ae49d7b29a3baffdd13
Antony Baker <antongorshenin@gmail.com>
2017-09-17 17:17:08 +0300

    Завершена работа над компонентом Input

--- src/frontend/js/components/navbar/index.jsx
+++ src/frontend/js/components/navbar/index.jsx
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { routeCodes } from 'config/routes';
 
+import Input from '../input'
 export default class Navbar extends Component {
   render() {
     return (
         <nav className="nav-extended">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo hide-on-med-and-down">Михаил Казиник</a>
                 <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-                <ul className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
-                </ul>
+                <div className="row hide-on-med-and-down">
+                    <div className="col s1"><a href="#" className="brand-logo hide-on-med-and-down">Михаил Казиник</a></div>
+                    <div className="col s1"><a href="sass.html">Sass</a></div>
+                    <div className="col s1"><a href="badges.html">Components</a></div>
+                    <div className="col s1"><a href="collapsible.html">JavaScript</a></div>
+                    <div className="col s1">6</div>
+                </div>
                 <ul className="side-nav" id="mobile-demo">
                     <li><a href="sass.html">Sass</a></li>
                     <li><a href="badges.html">Components</a></li>
