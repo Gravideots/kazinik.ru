1df6ef34c7402c59a10bf0c9d8638ff3107e0a45
Antony Baker <antongorshenin@gmail.com>
2017-09-17 19:39:23 +0300

    Реализован компонент Logo

---
+++ src/frontend/js/components/logo/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Text from 'components/text';
+
+export default class Logo extends Component {
+  static propTypes = {
+    children: PropTypes.object,
+  }
+
+  render() {
+    const {
+      children,
+      text
+    } = this.props;
+
+    let content = <Text type="header">{text}</Text>;
+
+    if(children)
+      content = children;
+
+    return (
+      <div className='Logo'>
+          {content}
+      </div>
+    );
+  }
+}
+
+//Usage
+// <Logo><Icon iconName={"alarm"} size='medium'/></Logo>
+//or
+// <Logo text='Михаил Казиник'/>
