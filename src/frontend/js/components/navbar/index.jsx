1f756d1f289eb9f9ea56990d5a66c493173add0a
Antony Baker <antongorshenin@gmail.com>
2017-09-17 18:44:31 +0300

    Подключение дополнительного шрифта и измененния в стилях.

--- src/frontend/js/components/navbar/index.jsx
+++ src/frontend/js/components/navbar/index.jsx
@@ -3,18 +3,21 @@ import { NavLink } from 'react-router-dom';
 import { routeCodes } from 'config/routes';
 
 import Input from '../input'
+
 export default class Navbar extends Component {
   render() {
     return (
         <nav className="nav-extended">
             <div className="nav-wrapper">
-                <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
-                <div className="row hide-on-med-and-down">
-                    <div className="col s1"><a href="#" className="brand-logo hide-on-med-and-down">Михаил Казиник</a></div>
-                    <div className="col s1"><a href="sass.html">Sass</a></div>
-                    <div className="col s1"><a href="badges.html">Components</a></div>
-                    <div className="col s1"><a href="collapsible.html">JavaScript</a></div>
-                    <div className="col s1">6</div>
+                <div className="row">
+                    <a href="#" data-activates="mobile-demo" className="col s1"><i className="material-icons">menu</i></a>
+                    <div className="row col s11 hide-on-med-and-down">
+                        <div className="col s1"><p>Михаил Казиник</p></div>
+                        <div className="col s1"><Input placeholder='Input' type='text' iconName='search' onChange={this.handleChange}/></div>
+                        <div className="col s1"><a href="badges.html">Components</a></div>
+                        <div className="col s1"><a href="collapsible.html">JavaScript</a></div>
+                        <div className="col s1">6</div>
+                    </div>
                 </div>
                 <ul className="side-nav" id="mobile-demo">
                     <li><a href="sass.html">Sass</a></li>
