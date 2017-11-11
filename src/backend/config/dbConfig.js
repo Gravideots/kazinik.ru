d3d0d08c54ec7f7dd8de6f3d9cbf53aa41d91ef0
TonyBaker <Antongorshenin@gmail.com>
2017-11-11 17:46:31 +0300

    DB config to remote server

--- src/backend/config/dbConfig.js
+++ src/backend/config/dbConfig.js
@@ -1,8 +1,8 @@
-const host = "localhost"
+const host = "mongodb://kazinik:KAZINIKDB@kazinik-shard-00-00-nol3s.mongodb.net:27017,kazinik-shard-00-01-nol3s.mongodb.net:27017,kazinik-shard-00-02-nol3s.mongodb.net:27017/test?ssl=true&replicaSet=Kazinik-shard-0&authSource=admin"
 const port = "27017"
 
 module.exports = {
     appDB: {
-        'url': 'mongodb://' + host + ':' + port + '/appDB'
+        'url': host
     }
 }
