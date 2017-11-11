0f2be69089ba2d6ac86f1da8ca934c4262d6da11
Nick Shtefan <nick.shtefan@gmail.com>
2017-11-11 19:59:33 +0300

    DB config cahnged. Добавлен удаленный хост

--- src/backend/config/dbConfig.js
+++ src/backend/config/dbConfig.js
@@ -1,7 +1,10 @@
-const host = "mongodb://kazinik:KAZINIKDB@kazinik-shard-00-00-nol3s.mongodb.net:27017,kazinik-shard-00-01-nol3s.mongodb.net:27017,kazinik-shard-00-02-nol3s.mongodb.net:27017/test?ssl=true&replicaSet=Kazinik-shard-0&authSource=admin"
-const port = "27017"
+const remoteHost = "mongodb://kazinik:KAZINIKDB@kazinik-shard-00-00-nol3s.mongodb.net:27017,kazinik-shard-00-01-nol3s.mongodb.net:27017,kazinik-shard-00-02-nol3s.mongodb.net:27017/test?ssl=true&replicaSet=Kazinik-shard-0&authSource=admin"
+const localHost = '127.0.0.1'
+const localPort = "27017"
 
-let dbpath = (process.env.DB === 'local')? 'mongodb://' + host + ':' + port + '/appDB' : "mongodb://kazinik:KAZINIKDB@kazinik-shard-00-00-nol3s.mongodb.net:27017,kazinik-shard-00-01-nol3s.mongodb.net:27017,kazinik-shard-00-02-nol3s.mongodb.net:27017/test?ssl=true&replicaSet=Kazinik-shard-0&authSource=admin";
+
+
+let dbpath = (process.env.DB === 'local') ? ('mongodb://' + localHost + ':' + localPort + '/appDB') :  remoteHost ;
 module.exports = {
     appDB: {
         'url': dbpath
