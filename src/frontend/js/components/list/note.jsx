5ae50d472e605636ed4cbb7e0ac4826e68622dfc
TonyBaker <Antongorshenin@gmail.com>
2018-01-05 21:19:58 +0300

    Формат времени

--- src/frontend/js/components/list/note.jsx
+++ src/frontend/js/components/list/note.jsx
@@ -9,11 +9,13 @@ import Button from 'components/button';
 export default class NoteListElement extends Component {
   render() {
     const {data, type} = this.props;
+    
+    if( !data ) return null;
 
     return(
       <div className='NoteListElement row'>
         <div className='col s12'>
-          <Text>{data.Author + " " + data.Date.toLocaleString('ru-RU')}</Text>
+          <Text>{data.Author + " " + ( data.Date ? data.Date.toLocaleDateString('ru-RU') : "" ) } </Text>
           <Image src={data.TitleImage.Crop}/>
           <Text type='helptext'>{data.SubTitle.slice(0, 400) + '...'}</Text>
         </div>
